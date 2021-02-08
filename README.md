4dc7a981dd4474a69b0050059d651945b89c0a94
Brian Fioca <bfioca@gmail.com>
2021-02-08 13:28:40 -0800

    committing open source

---
+++ README.md
@@ -0,0 +1,9 @@
+# RescueTime for VS Code
+
+The RescueTime for VS Code extension makes it easy to stay aware of your software development time without leaving your editor. As you work, you will see a total of your daily software development time and a gauge of how focused you currently are on coding.
+
+## What's this repo?
+
+This is the source code for the RescueTime VS Code extension that you can find [here in the Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=RescueTime.rescuetime).
+
+It's provided as an example implementation of the [RescueTime API](https://www.rescuetime.com/anapi/manage) and can be used as inspiration for other projects of a similar nature.
---
+++ src/extension.ts
@@ -0,0 +1,126 @@
+import * as vscode from 'vscode';
+import { window } from 'vscode';
+
+let rp = require('request-promise');
+
+let myStatusBarItem: vscode.StatusBarItem;
+let apiKey: String | undefined;
+let hoursMinutes: String | undefined;
+let focusPercentage: number | undefined;
+let focusDots: String | undefined;
+let tooltipText: String | undefined;
+let extensionContext: vscode.ExtensionContext;
+
+const REFRESH_TIME = 60 * 1000;
+const RETRY_TIME = 30 * 1000;
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('RescueTime: is now active...');
+
+  extensionContext = context;
+
+  const myCommandId = 'rescuetime.openDashboard';
+
+  myStatusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+  myStatusBarItem.command = myCommandId;
+  context.subscriptions.push(myStatusBarItem);
+
+  context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+    if (apiKey) {
+      if (focusPercentage) {
+        window.showInformationMessage(`You are currently ${focusPercentage}% focused on this.`);
+      }
+      window.showInformationMessage(`You've spent ${hoursMinutes} on Software Development today.`);
+    } else {
+      resetApiKey();
+    }
+  }));
+
+  // check to see if API key is set, if not ask for it
+  apiKey = context.globalState.get('rescuetime.apiKey');
+  if (apiKey !== undefined && apiKey.trim() !== '') {
+    // update status bar
+    updateStatusBarItem();
+  } else {
+    resetApiKey();
+  }
+}
+
+function updateStatusBarItem(): void {
+  hoursMinutes = "-h -m";
+  rp({
+      method: 'GET',
+      uri: 'https://www.rescuetime.com/api/data/taxonomy_presence_summary',
+      qs: { taxonomy_name: 'overview', taxon_id: 10 },
+      headers: { 'Authorization' : 'Bearer ' + apiKey },
+      json: true
+    }).then((response: any) => {
+      let result = response;
+      hoursMinutes = getDurationString(response.duration);
+      focusPercentage = response.focus_percentage;
+      focusDots = getFocusDots(focusPercentage);
+      tooltipText = getTooltipText(focusPercentage);
+      myStatusBarItem.text = `Dev Time $(clock): ${hoursMinutes} ${focusDots}`;
+      myStatusBarItem.tooltip = `${tooltipText}`;
+      myStatusBarItem.show();
+      setTimeout(function(){ updateStatusBarItem(); }, REFRESH_TIME);
+    }).catch((e: any) => {
+      console.log(`RescueTime: Error: ${e}`);
+      if (e && e.error && e.error.error && e.error.error.includes('key not')) {
+        resetApiKey();
+      }
+      else {
+        console.log('RescueTime: Retrying in 30 seconds..');
+        setTimeout(function(){ updateStatusBarItem(); }, RETRY_TIME);
+      }
+    });
+}
+
+function getDurationString(duration: number): String {
+  let date = new Date(0);
+  date.setSeconds(duration);
+  let timeString = date.toISOString().substr(11, 8);
+  let hoursMinsSecs = timeString.split(':');
+  if (parseInt(hoursMinsSecs[0]) > 0) {
+    timeString = `${parseInt(hoursMinsSecs[0])}h ${parseInt(hoursMinsSecs[1])}m`;
+  } else {
+    timeString = `${parseInt(hoursMinsSecs[1])}m`;
+  }
+  return timeString;
+}
+
+function getFocusDots(percentage: number | undefined): String {
+  if (!percentage) { return ''; }
+  let solidDots = Math.round(percentage / 20.0);
+  let result = ['○','○','○','○','○'];
+  for (let i = 0; i < solidDots; i++) {
+    result[i] = '◉';
+  }
+  return result.join('');
+}
+
+function getTooltipText(percentage: number | undefined): String {
+  if (!percentage) { return ''; }
+  let index = Math.round(percentage / 20.0);
+  let focusLevels = ['Not Focused', 'Focus: Low', 'Focus: Mild', 'Focus: Moderate', 'Focus: High', 'Focus: Very High'];
+  return focusLevels[index];
+}
+
+function resetApiKey() {
+  extensionContext.globalState.update('rescuetime.apiKey', undefined);
+  myStatusBarItem.text = `Dev Time $(clock): (click to enter API key)`;
+  myStatusBarItem.tooltip = `Click to enter your RescueTime API key`;
+  myStatusBarItem.show();
+
+  window.showInputBox({ placeHolder: "Enter your RescueTime API Key" }).then(value => {
+    if (!value) {
+      apiKey = undefined;
+      return;
+    }
+    apiKey = value.trim();
+    extensionContext.globalState.update('rescuetime.apiKey', apiKey);
+    updateStatusBarItem();
+  });
+}
+
+export function deactivate() {}
